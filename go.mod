ebbd7becc01ef8261bcdd3098724608cf5ee84b0
Jeremy Busk <jeremybusk@gmail.com>
2021-01-10 03:26:32 +0000

    update

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/jeremybusk/unodeutils
 
 go 1.15
-
-require (
-	rsc.io/quote/v3 v3.1.0
-	rsc.io/sampler v1.99.99 // indirect
-)
--- hello.go
+++ hello.go
@@ -1,19 +1,5 @@
-package unodeutils 
-
-// import "rsc.io/quote/v3"
+package hello
 
 func Hello() string {
-    // return quote.HelloV3()
-    return ("Hello, world")
-}
-
-func Proverb() string {
-    // return quote.Concurrency()
-    return ("Hello, world")
-}
-
-func Hi() string {
-    // return (println("Hello, world"))
-    return ("Hello, world")
-    // println("Hello, world")
+    return "Hello, world."
 }
